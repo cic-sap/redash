5912c5e23689607d40ca6b96788861c9b916f248
sdm <dongming.shen@sap.com>
2021-11-09 18:44:31 +0800

    height

--- client/app/components/dashboards/EmailDashboardGrid.jsx
+++ client/app/components/dashboards/EmailDashboardGrid.jsx
@@ -75,7 +75,7 @@ const EmailDashboardWidget = React.memo(
       );
     }
     if (type === WidgetTypeEnum.TEXTBOX) {
-      return <TextboxWidget widget={widget} tableLayout={tableLayout} canEdit={canEdit} isPublic={isPublic} onDelete={onDelete}/>;
+      return <TextboxWidget widget={widget} height={height} tableLayout={tableLayout} canEdit={canEdit} isPublic={isPublic} onDelete={onDelete}/>;
     }
     return <RestrictedWidget widget={widget} tableLayout={tableLayout}  />;
   },
